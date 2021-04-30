2b5d0f6660bce6b2813f1e066ec01c5d9e97781d
Mike <mczacierka@gmail.com>
2021-04-30 14:37:38 -0400

    cmdline flags

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/zacierka/csgodemoparser/internal/parser"
+import (
+	"flag"
+
+	"github.com/zacierka/csgodemoparser/internal/parser"
+)
+
+var path string
+
+func init() {
+	flag.StringVar(&path, "p", "example/matches/match730_003478395714113896624_1370236049_121.dem", "Path to demo")
+}
 
 func main() {
-	path := "example/matches/match730_003478395714113896624_1370236049_121.dem"
 	parser.ParseDemo(path)
 }
