aac62bba27185f76586a10fa69d4f10834a5d74f
Mike <mczacierka@gmail.com>
2021-05-23 02:39:32 -0400

    gorm impl

--- internal/parser/results_export.go
+++ internal/parser/results_export.go
@@ -2,34 +2,56 @@ package parser
 
 import (
 	"fmt"
-	"sort"
+	"log"
 
 	common "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/common"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var teamwins int = 0
 var enemywins int = 0
 var members [5]string
+var db *gorm.DB = connect()
+
+type Match struct {
+	ID          int
+	MatchID     string
+	Map         string
+	ScoreT      int
+	ScoreE      int
+	Result      uint8 // 0 = loss, 1 = win, 2 = tie
+	Playerone   string
+	Playertwo   string
+	Playerthree string
+	Playerfour  string
+	Playerfive  string
+}
 
 func (p *DemoParser) ProcessResults() {
-	fmt.Println("---------------------")
+	setupDatabase(db)
 	res := p.isEBCWin()
-	var result string = "LOSS"
-	if res {
-		result = "WIN"
+
+	match := Match{
+		MatchID:     p.Match.ID,
+		Map:         p.Match.Map,
+		ScoreT:      teamwins,
+		ScoreE:      enemywins,
+		Result:      res, // loss
+		Playerone:   normalizePlayer(p.Match.Players[1].SteamID),
+		Playertwo:   normalizePlayer(p.Match.Players[1].SteamID),
+		Playerthree: normalizePlayer(p.Match.Players[1].SteamID),
+		Playerfour:  normalizePlayer(p.Match.Players[1].SteamID),
+		Playerfive:  normalizePlayer(p.Match.Players[1].SteamID),
 	}
-	if teamwins < 16 {
-		fmt.Printf("Scoreboard: %d-%d %s\n", enemywins, teamwins, result)
-	} else {
-		fmt.Printf("Scoreboard: %d-%d %s\n", teamwins, enemywins, result)
-	}
-	fmt.Print("Team Members:")
-	fmt.Println(sort.StringSlice(members[:]))
-	fmt.Println("---------------------")
+	// fmt.Println(sort.StringSlice(members[:]))
+
+	id := insertMatch(&match)
+	log.Printf("\tresults_export::ProcessResults() inserted match with id %d\n", id)
 }
 
 // Team 2T 3CT
-func (p *DemoParser) isEBCWin() bool {
+func (p *DemoParser) isEBCWin() uint8 {
 	var team common.Team = common.Team(1)
 	var idx int = 0
 	for _, pl := range p.Match.Players {
@@ -49,10 +71,34 @@ func (p *DemoParser) isEBCWin() bool {
 	}
 
 	if teamwins == 16 {
-		return true
+		return 1
 	} else if teamwins < 16 {
-		return false
+		return 0
 	} else {
-		return true
+		return 2
 	}
+}
+
+func connect() *gorm.DB {
+	dsn := "SQL/PATH/HERE"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	return db
+}
+
+func setupDatabase(db *gorm.DB) {
+	db.AutoMigrate(&Match{})
+}
+
+func insertMatch(match *Match) int {
+	db.Create(&match)
+	return match.ID
+}
+
+func normalizePlayer(steamid uint64) string {
+	val := fmt.Sprint(steamid)
+	return val
 }
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 var path string
 
 func init() {
-	flag.StringVar(&path, "p", "example/matches/match730_003478395714113896624_1370236049_121.dem", "Path to demo")
+	flag.StringVar(&path, "p", "example/matches/match730_003469709776265413104_0116120779_129.dem", "Path to demo")
 }
 
 func main() {
@@ -24,4 +24,5 @@ func main() {
 
 	log.Printf("\tProcessing Results for game ID: %s", parser.Match.ID)
 	parser.ProcessResults()
+	log.Printf("\tFinished Processing ID: %s", parser.Match.ID)
 }
